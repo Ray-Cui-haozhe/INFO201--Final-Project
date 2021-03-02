c7a207a11114d969563c9164b04184a730c649c9
lzhou4 <lzhou4@uw.edu>
2021-03-02 21:07:05 +0800

    fixed table heading

--- Aggregate_Table.R
+++ Aggregate_Table.R
@@ -7,7 +7,7 @@ df <- read.csv("combined_final_last_10_years.csv")
 value <- df %>%
   group_by(continent) %>%
   select(c("demox_eiu", "income_per_person", "gini_index")) %>%
-  summarise(democracy_index = mean(demox_eiu),
-            income_per_person = mean(income_per_person),
-            gini_index = mean(gini_index))
-value <- value[order(value$gini_index),]
+  summarise("Democracy Index" = mean(demox_eiu),
+            "Income per Person" = mean(income_per_person),
+            "Gini Index" = mean(gini_index))
+value <- value[order(value$`Gini Index`),]
